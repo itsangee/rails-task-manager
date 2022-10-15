74979a3bf40bd4e599e7ab633f1c7864d0e9978f
angelee otic <otic.angeleemae@gmail.com>
2022-10-15 15:16:03 +0100

    crud rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+4.times do
+  Task.create(title: "Studying", details: "A lot of flashcards to do", completed: true)
+end
 #   Character.create(name: "Luke", movie: movies.first)
